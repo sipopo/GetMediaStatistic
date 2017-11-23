e041c8b8caab0c241ba3e9ab7fc5d7038dba41c1
Sergey Osipov <sipopo@yandex.ru>
2017-11-23 10:03:27 +0300

    1.  Add synchrobe statistic 2.  Rewite UpdateDB function

--- GetMediaStatistic/Program.cs
+++ GetMediaStatistic/Program.cs
@@ -30,38 +30,48 @@ namespace GetMediaStatistic
             }
         } // stop Class Declaring
 
+
+        private class MR_statistic
+        {
+            public string Name;
+            public string Value;
+        }
+
+        /*
         private class Synrob
         {
+            public int MAX_tt;
+            public int MIN_tt;
+            public int AVG_tt;
 
+            public string description;
         }
+        */
 
         static void Main(string[] args)
         {
-            Log("Start Program");
-            Accounts accounts = new Accounts();
+
+            List<MR_statistic> MR_statistics = new List<MR_statistic>();
+
+            Log("Start Program");          
 
             Log("Select from BranchDB");
-            // accounts = GetAccountsInBranch();
+            MR_statistics.AddRange(GetAccountsInBranch());
 
-            /*
-            Log(String.Format("OTT :{0}, STB :{1}, CTI :{2}, b2b :{3}, others :{4}, ALL :{5}",
-                    accounts.OTT, accounts.STB, accounts.CTI, accounts.b2b, accounts.others,
-                    accounts.All()));
+            Log("Select from Synchrobe");
+            MR_statistics.AddRange(GetSynchrobeStat());
 
+            Log("Statistics information:");
+            foreach (var MR_statistic in MR_statistics)
+            {
+                Console.WriteLine(MR_statistic.Name + ":" + MR_statistic.Value);                
+                Log(String.Format(" {0}: {1} ", MR_statistic.Name,MR_statistic.Value));
+            }
 
-            Console.WriteLine("OTT :{0}, STB :{1}, CTI :{2}, b2b :{3}, others :{4}, ALL :{5}",
-                    accounts.OTT, accounts.STB, accounts.CTI, accounts.b2b, accounts.others,
-                    accounts.All());
-            */
-
-
-
-            /* stop for a monent 
-            
             //check sqlite file
             string path = Properties.Settings.Default.TMPPath;
             string filename = "\\MediaroomStatistic.sqlite";
-                        
+
             string sqlfile = path + filename;
             if (!File.Exists(sqlfile))
             {
@@ -69,34 +79,43 @@ namespace GetMediaStatistic
                 Log("Create SQLlite file DB");
                 CreateSQLiteDB(sqlfile);
             }
-            
-            Log("Add information to SQLite DB");
-            UpdateSQLiteDB(accounts, sqlfile);
 
+            Log("Add information to SQLite DB");
+            UpdateSQLiteDB(MR_statistics, sqlfile);
+
+            
             Log("Copy file to web server");
             string uncpath = Properties.Settings.Default.DataPath;
 
-            try {
+            try
+            {
                 File.Copy(sqlfile, uncpath + filename, true);
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine("Error with copy file to uncpath" + e.ToString());
                 Log("Error with copy file to uncpath" + e.ToString());
-            } 
-           
-            */
+            }
+            
 
-            GetSynchrobeStat();
-
-            Console.ReadLine();
+            //Console.ReadLine();
             Log("Stop Program");
 
         }// main
 
 
-        static void GetSynchrobeStat()
+        static List<MR_statistic> GetSynchrobeStat()
         {
-            string[] servernames = { "MSKRUSSTBSYN001", "MSKRUSSTBSYN002" };
+            //string[] servernames = { "MSKRUSSTBSYN001", "MSKRUSSTBSYN002" };
+            //string[] times = { "00:15:00", "00:30:00", "01:00:00" };
+            Dictionary<string,string> servers = new Dictionary<string, string>();
+            servers.Add("synh01", "MSKRUSSTBSYN001");
+            servers.Add("synh02", "MSKRUSSTBSYN002");
+
+            Dictionary<string, string> times = new Dictionary<string, string>();
+            times.Add("15m", "00:15:00");
+            times.Add("30m", "00:30:00");
+            times.Add("1h", "01:00:00");
 
             string LogPath = @"\c$\inetpub\logs\LogFiles\W3SVC3";            
             string FileName = "u_ex" + DateTime.Now.ToString("yyMMdd") + "*.log";
@@ -107,20 +126,44 @@ namespace GetMediaStatistic
             LogParser = new LogQueryClass();
             IISLog = new COMIISW3CInputContextClass();
 
+            List<MR_statistic> MR_statistics = new List<MR_statistic>();            
             string strSQL = null;
 
             try
             {
-                foreach (string server in servernames)
-                    { 
+                foreach (var server in servers)
+                { 
+                    foreach (var time in times)                   
+                    {
 
-                    strSQL =  " SELECT MAX(time-taken) as MAX_tt, MIN(time-taken) as MIN_tt, AVG(time-taken) as AVG_tt " +
-                          " FROM \\\\" + server + "\\" + LogPath + "\\" + FileName +
-                          " WHERE time > SUB( SYSTEM_TIME(), TO_TIMESTAMP('00:15:00','HH:mm:ss') )  ";
-                    // Console.WriteLine(strSQL);
-                    // run query
-                    ILogRecordset rsLP = LogParser.Execute(strSQL, IISLog);
-                    Console.WriteLine(rsLP.getRecord().getValue("MAX_tt"));
+                        strSQL =  " SELECT MAX(time-taken) as MAX_tt, MIN(time-taken) as MIN_tt, AVG(time-taken) as AVG_tt " +
+                                      " FROM \\\\" + server.Value + "\\" + LogPath + "\\" + FileName +
+                                      " WHERE time > SUB( SYSTEM_TIME(), TO_TIMESTAMP('" + time.Value + "','HH:mm:ss') )  ";
+
+                        //Console.WriteLine(strSQL);
+                        // run query
+                        ILogRecordset rsLP = LogParser.Execute(strSQL, IISLog);
+                        /*
+                        Console.WriteLine(rsLP.getRecord().getValue("MAX_tt"));
+                        Console.WriteLine(rsLP.getRecord().getValue("MIN_tt"));
+                        Console.WriteLine(rsLP.getRecord().getValue("AVG_tt"));
+                        */                        
+                        MR_statistic MR_statistic = new MR_statistic();
+                        MR_statistic.Name = server.Key + "_" + time.Key + "_" + "MaxTT";
+                        MR_statistic.Value = Convert.ToString(rsLP.getRecord().getValue("MAX_tt"));
+                        MR_statistics.Add(MR_statistic);   
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Name = server.Key + "_" + time.Key + "_" + "MinTT";
+                        MR_statistic.Value = Convert.ToString(rsLP.getRecord().getValue("MIN_tt"));
+                        MR_statistics.Add(MR_statistic);
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Name = server.Key + "_" + time.Key + "_" + "AvgTT";
+                        MR_statistic.Value = Convert.ToString(rsLP.getRecord().getValue("AVG_tt"));
+                        MR_statistics.Add(MR_statistic);                      
+                    }
+                    //Console.WriteLine("---");
                 }// foreach                              
             }
             catch (Exception ex)
@@ -129,10 +172,11 @@ namespace GetMediaStatistic
                 Console.WriteLine("Something wrong: {0}", ex.Message);
                // SendFlag = 0;
 
-            }            
+            }
+            return MR_statistics;
         }// End of GetSynchrobeStat
 
-        static void UpdateSQLiteDB(Accounts accounts, string sqlfile)
+        static void UpdateSQLiteDB(List<MR_statistic> MR_Statistics, string sqlfile)
         {
             try
             {
@@ -141,7 +185,7 @@ namespace GetMediaStatistic
                     //sqlfile = "\\mskrusmdsif004\c$\inetpub\wwwroot\ShowMediaroomStatistic\App_Data\MediaroomStatistic.db";
                     sqlite.ConnectionString = "Data Source=" + sqlfile;
                     sqlite.Open();
-
+                    /*
                     string query = "REPLACE into MR_statistics (Name, Value, UpdateDate) VALUES " +
                                    "('OTT_accounts','" + accounts.OTT.ToString() + "', DateTime('now') )," +
                                    "('CTI_accounts','" + accounts.CTI.ToString() + "', DateTime('now') )," +
@@ -149,11 +193,15 @@ namespace GetMediaStatistic
                                    "('Others_accounts', '" + accounts.others.ToString() + "', DateTime('now') )," +
                                    "('ALL_accounts', '" + accounts.All().ToString() + "', DateTime('now') ), " +
                                    "('STB_accounts','" + accounts.STB.ToString() + "', DateTime('now') );";
+                    */
+                    foreach (var MR_statistic in MR_Statistics)
+                    {
+                        string query = "REPLACE into MR_statistics (Name, Value, UpdateDate) VALUES " +
+                                       "('"+ MR_statistic.Name + "','" + MR_statistic.Value + "', DateTime('now') );";
 
-                    SQLiteCommand command = new SQLiteCommand(query, sqlite);
-                    command.ExecuteNonQuery();
-                    
-
+                        SQLiteCommand command = new SQLiteCommand(query, sqlite);
+                        command.ExecuteNonQuery();
+                    }                                                            
                 }
 
             } catch (Exception e)
@@ -190,9 +238,11 @@ namespace GetMediaStatistic
         }
 
 
-        static Accounts GetAccountsInBranch()
+        static List<MR_statistic> GetAccountsInBranch()
         {
             Accounts accounts = new Accounts();
+            List <MR_statistic> MR_statistics = new List<MR_statistic>();
+
             try
             {
                 using (SqlConnection conn = new SqlConnection())
@@ -210,6 +260,7 @@ namespace GetMediaStatistic
 
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
+                        MR_statistic MR_statistic = new MR_statistic();
                         while (reader.Read())
                         {
                             //Console.WriteLine( string.Format("{0} \t | {1} ",reader[0],reader[1]) );
@@ -238,6 +289,35 @@ namespace GetMediaStatistic
                             }// switch
 
                         }
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Value = Convert.ToString(accounts.others);
+                        MR_statistic.Name = "Others_accounts";
+                        MR_statistics.Add(MR_statistic);
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Value = Convert.ToString(accounts.All().ToString());
+                        MR_statistic.Name = "ALL_accounts";
+                        MR_statistics.Add(MR_statistic);
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Value = Convert.ToString(accounts.b2b);
+                        MR_statistic.Name = "b2b_accounts";
+                        MR_statistics.Add(MR_statistic);
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Value = Convert.ToString(accounts.CTI);
+                        MR_statistic.Name = "CTI_accounts";
+                        MR_statistics.Add(MR_statistic);
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Value = Convert.ToString(accounts.STB);
+                        MR_statistic.Name = "STB_accounts";
+                        MR_statistics.Add(MR_statistic);
+
+                        MR_statistic = new MR_statistic();
+                        MR_statistic.Value = Convert.ToString(accounts.OTT);
+                        MR_statistic.Name = "OTT_accounts";
+                        MR_statistics.Add(MR_statistic);
                     } //using              
                 } // using
             }
@@ -247,13 +327,13 @@ namespace GetMediaStatistic
                 Console.WriteLine("Error with BranchDB " + e.ToString());
                 //Console.ResetColor();                
             }
-            return accounts;
+            return MR_statistics;
         } // 
 
         public static void Log(string logMessage)
         {
-            //string path = Properties.Settings.Default.LogPath;
-            string path = ".\\";
+            string path = Properties.Settings.Default.LogPath;
+            //string path = ".\\";
 
 
             string filename = Process.GetCurrentProcess().ProcessName + "_" + DateTime.Today.ToString("yyyy-MM-dd") + ".log";
